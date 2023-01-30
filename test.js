84b2fdcc31711d6f19bef2d28126557b296fad27
shashank <shashank@UBUNTU.myguest.virtualbox.org>
2023-01-30 10:00:16 +0530

    deleted B changes

--- test.js
+++ test.js
@@ -16,7 +16,5 @@ function factorial(n) {
 let n = 4;
 answer = factorial(n)
 console.log("Factorial of " + n + " : " + answer);
-console.log("Print B")
 console.log("Print A")
 console.log("Print A")
-console.log("Print B")
